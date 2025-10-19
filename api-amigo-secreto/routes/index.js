58a44b83c7c0c2237fda320d5ca07e1910d2aeff
richardparra99 <richardparra.068@gmail.com>
2025-10-18 23:28:52 -0400

    se elimino proyecto api

--- api-amigo-secreto/routes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = app => {
-    require('./persona.routes')(app);
-    require("./search.routes")(app);
-    require("./auth.routes")(app);
-};
--- api-amigo-secreto/routes/search.routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-    let router = require("express").Router();
-    const controller = require("../controllers/search.controller");
-
-    router.get("/", controller.globalSearch);
-    app.use("/search", router);
-}
